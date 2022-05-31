e90f5d6378daeb8fd640e7a0584da88da596a798
mizza-cz <mizza.cz666@gmail.com>
2022-05-31 10:05:22 +0200

    add hp

---
+++ src/components/newsboard-section/newsboard-section.js
@@ -0,0 +1,13 @@
+// $(".play-video-modal").on("click", function (e) {
+//    e.preventDefault();
+//    var n = $(this).data("video"),
+//        a = $(
+//            '<div class="video-modal modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">                 <div class="modal-dialog modal-lg">                     <div class="modal-content">                     <div class="embed-responsive embed-responsive-16by9">                         <iframe width="424" height="238" src="https://www.youtube.com/embed/' +
+//                o(n) +
+//                '?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                     </div>                     </div>                 </div>             </div>'
+//        );
+//    $("body").append(a), $(".video-modal.hide").show(), $(".video-modal").modal();
+// }),
+// $(document).on("hidden.bs.modal", ".video-modal", function () {
+//    $(this).remove();
+// });
